831aa5a04047d7319a2c625203ee102a626e93ae
Lukas Frensel <Lukas.Frensel@exxeta.com>
2021-04-08 12:03:02 +0200

    Add errorhandling for non existing grahpite service

--- scripts/configure_telegraf.sh
+++ scripts/configure_telegraf.sh
@@ -20,10 +20,16 @@ getGraphitePort()
 GRAPHITE_URL=$(getGraphiteUrl)
 GRAPHITE_PORT=$(getGraphitePort)
 
+if [ $GRAPHITE_URL == null -o $GRAPHITE_URL == "" -o $GRAPHITE_PORT == null -o $GRAPHITE_PORT == ""]; then
+  echo "       **ERROR** No Graphite configuration found in Services!"
+  echo "                 Please add the a9s_Prometheus Service to use this buildpack!"
+  exit 1
+fi
+
 sed -i 's/localhost:2003/'$GRAPHITE_URL':'$GRAPHITE_PORT'/' $TELEGRAF_CONF_FILE
 
 echo "GraphiteURL $GRAPHITE_URL:$GRAPHITE_PORT!"
 
-sed -i 's/9273/9100/' $TELEGRAF_CONF_FILE
+# sed -i 's/9273/9100/' $TELEGRAF_CONF_FILE
 
-cat $TELEGRAF_CONF_FILE
+# cat $TELEGRAF_CONF_FILE
