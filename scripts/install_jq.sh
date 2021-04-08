5719198388c11de06a46b350dc4aacfdd02f6dd4
Lukas Frensel <Lukas.Frensel@exxeta.com>
2021-04-08 11:41:17 +0200

    add sha check add variable to modul installer

--- scripts/install_jq.sh
+++ scripts/install_jq.sh
@@ -9,7 +9,7 @@ VERSION="1.6"
 URL="https://github.com/stedolan/jq/releases/download/jq-${VERSION}/jq-linux64"
 
 if [ $CF_STACK == "cflinuxfs3" ]; then
-    SHA256="a3a8b950c7dc63c03e46140f2abd016cc31cbfff02f5152d5c85d80fef4295e4"
+    SHA256="af986793a515d500ab2d35f8d2aecd656e764504b789b66d7e1a0b727a124c44"
 else
   echo "       **ERROR** Unsupported stack"
   echo "                 See https://docs.cloudfoundry.org/devguide/deploy-apps/stacks.html for more info"
@@ -27,15 +27,11 @@ if [ ! -f $InstallDir/jq-linux64 ]; then
   curl -s -L --retry 15 --retry-delay 2 $URL -o $InstallDir/jq-linux64
 
   DOWNLOAD_SHA256=$(shasum -a 256 $InstallDir/jq-linux64 | cut -d ' ' -f 1)
-  echo "-----> SHA: ${DOWNLOAD_SHA256}"
-#   if [[ $DOWNLOAD_SHA256 != $SHA256 ]]; then
-#     echo "       **ERROR** SHA256 mismatch: got $DOWNLOAD_SHA256 expected $SHA256"
-#     exit 1
-#   fi
 
-#   tar xfz /tmp/module.tar.gz -C $DepDir
-#   mv "$DepDir/node_exporter-${VERSION}.linux-amd64" $InstallDir
-#   rm /tmp/module.tar.gz
+  if [[ $DOWNLOAD_SHA256 != $SHA256 ]]; then
+    echo "       **ERROR** SHA256 mismatch: got $DOWNLOAD_SHA256 expected $SHA256"
+    exit 1
+  fi
 
   mkdir -p "$DepDir/bin"
   cd $DepDir/bin;
