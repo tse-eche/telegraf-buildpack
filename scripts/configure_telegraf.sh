b21cb39ada35e6f54115f9f1f0c75bcd710c55ad
Lukas Frensel <Lukas.Frensel@exxeta.com>
2021-04-28 13:05:04 +0200

    comment out too much

--- scripts/configure_telegraf.sh
+++ scripts/configure_telegraf.sh
@@ -32,20 +32,20 @@ getGraphitePort()
 
 # echo "-----> GraphiteURL: '$GRAPHITE_URL:$GRAPHITE_PORT'"
 
-# if [ -z ${PROM_HOST+x} ]; 
-# then 
-#   export PROM_HOST="localhost"
-# fi
+if [ -z ${PROM_HOST+x} ]; 
+then 
+  export PROM_HOST="localhost"
+fi
 
-# if [ -z ${PROM_PORT+x}  ]; 
-# then 
-#   export PROM_PORT=9100
-# fi
+if [ -z ${PROM_PORT+x}  ]; 
+then 
+  export PROM_PORT=9100
+fi
 
-# if [ -z ${PROM_PATH+x}  ]; 
-# then 
-#   export PROM_PATH="metrics"
-# fi
+if [ -z ${PROM_PATH+x}  ]; 
+then 
+  export PROM_PATH="metrics"
+fi
 
 sed -i 's|localhost:9100/metrics|'$PROM_HOST':'$PROM_PORT'/'$PROM_PATH'|' $TELEGRAF_CONF_FILE
 
