07c4d260c35d2c5aabf8788bc5477f6e9ff9e0ef
Lukas Frensel <Lukas.Frensel@exxeta.com>
2021-05-10 10:19:19 +0200

    reconfigure a lot of stuff in configuration support application name

--- scripts/configure_telegraf.sh
+++ scripts/configure_telegraf.sh
@@ -9,6 +9,11 @@ echo "-----> Configuring telegraf"
 
 TELEGRAF_CONF_FILE=$DEPS_DIR/$DEPS_IDX/telegraf/telegraf.conf
 
+getApplicationName()
+{
+    echo $(echo $VCAP_APPLICATION | jq -r '.application_name')
+}
+
 getGraphiteUrl()
 {
     echo $(echo $VCAP_SERVICES | jq -r '."a9s-prometheus"[0].credentials."graphite_exporters"[0]')
@@ -19,18 +24,34 @@ getGraphitePort()
     echo $(echo $VCAP_SERVICES | jq '."a9s-prometheus"[0].credentials."graphite_exporter_port"')
 }
 
-# GRAPHITE_URL=$(getGraphiteUrl)
-# GRAPHITE_PORT=$(getGraphitePort)
+if [ -z ${APPLICATION_NAME+x} ]; 
+then 
+  export APPLICATION_NAME=$(getApplicationName)
+fi
 
-# if [ ${GRAPHITE_URL} == "null" ]; then
-#   echo "       **ERROR** No Graphite configuration found in Services!"
-#   echo "                 Please add the a9s_Prometheus Service to use this buildpack!"
-#   exit 1
-# fi
+sed -i 's|dhc_application_name|'$APPLICATION_NAME'|' $TELEGRAF_CONF_FILE
 
-# sed -i 's/localhost:2003/'$GRAPHITE_URL':'$GRAPHITE_PORT'/' $TELEGRAF_CONF_FILE
+echo "-----> Application Name in Graphite: '$APPLICATION_NAME'"
 
-# echo "-----> GraphiteURL: '$GRAPHITE_URL:$GRAPHITE_PORT'"
+if [ -z ${GRAPHITE_URL+x} ]; 
+then 
+  export GRAPHITE_URL=$(getGraphiteUrl)
+fi
+
+if [ -z ${GRAPHITE_PORT+x} ]; 
+then 
+  export GRAPHITE_PORT=$(getGraphitePort)
+fi
+
+if [ ${GRAPHITE_URL} == "null" ]; then
+  echo "       **ERROR** No Graphite configuration found in Services!"
+  echo "                 Please add the a9s_Prometheus Service to use this buildpack!"
+  exit 1
+fi
+
+sed -i 's/localhost:2003/'$GRAPHITE_URL':'$GRAPHITE_PORT'/' $TELEGRAF_CONF_FILE
+
+echo "-----> GraphiteURL: '$GRAPHITE_URL:$GRAPHITE_PORT'"
 
 if [ -z ${PROM_HOST+x} ]; 
 then 
