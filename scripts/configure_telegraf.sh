54ac5eb7e6960aea0c0a519b3e4a38c7588a3b48
Lukas Frensel <Lukas.Frensel@exxeta.com>
2021-05-11 12:42:54 +0200

    another try to fix hole config

--- scripts/configure_telegraf.sh
+++ scripts/configure_telegraf.sh
@@ -62,8 +62,8 @@ if [ ${NO_PROM} == "true" ];
 then 
 
   sed -i 's/\[\[inputs.prometheus\]\]/# \[\[inputs.prometheus\]\]/' $TELEGRAF_CONF_FILE
-  sed -i 's|"urls = [\"http://localhost:9100/metrics\"]"|"# urls = [\"http://localhost:9100/metrics\"]"|' $TELEGRAF_CONF_FILE
-  sed -i 's|" metric_version = 2"|"# metric_version = 2"|' $TELEGRAF_CONF_FILE
+  sed -i 's|urls = \[\"http://localhost:9100/metrics\"\]|# urls = \[\"http://localhost:9100/metrics\"\]|' $TELEGRAF_CONF_FILE
+  sed -i 's/ metric_version = 2/# metric_version = 2/' $TELEGRAF_CONF_FILE
 
   echo "-----> Skip Prometheus Configuration"
 
