313cfa27ca867bdf631cdc26cfd77fdbb86299f0
Lukas Frensel <Lukas.Frensel@exxeta.com>
2021-06-14 11:13:07 +0200

    Change to online installation

--- scripts/install_module.sh
+++ scripts/install_module.sh
@@ -7,11 +7,11 @@ DEPS_IDX=$2
 
 echo "-----> Install $SIDECAR_NAME binaries"
 
-VERSION="1.18.0"
+VERSION="1.18.3"
 URL="https://dl.influxdata.com/telegraf/releases/telegraf-${VERSION}_linux_amd64.tar.gz"
 
 if [ $CF_STACK == "cflinuxfs3" ]; then
-    SHA256="a3a8b950c7dc63c03e46140f2abd016cc31cbfff02f5152d5c85d80fef4295e4"
+    SHA256="8fa528ad53d4e23bf52e0abb93125134b4f46755b2f28331c27119a078542f9c"
 else
   echo "       **ERROR** Unsupported stack"
   echo "                 See https://docs.cloudfoundry.org/devguide/deploy-apps/stacks.html for more info"
@@ -23,21 +23,21 @@ InstallDir="$DepDir/telegraf"
 
 if [ ! -f $InstallDir/telegraf ]; then
   
-  # echo "-----> Download telegraf ${VERSION}"
-  # curl -s -L --retry 15 --retry-delay 2 $URL -o /tmp/module.tar.gz
+  echo "-----> Download telegraf ${VERSION}"
+  curl -s -L --retry 15 --retry-delay 2 $URL -o /tmp/module.tar.gz
 
-  # DOWNLOAD_SHA256=$(shasum -a 256 /tmp/module.tar.gz | cut -d ' ' -f 1)
+  DOWNLOAD_SHA256=$(shasum -a 256 /tmp/module.tar.gz | cut -d ' ' -f 1)
 
-  # if [[ $DOWNLOAD_SHA256 != $SHA256 ]]; then
-  #   echo "       **ERROR** SHA256 mismatch: got $DOWNLOAD_SHA256 expected $SHA256"
-  #   exit 1
-  # fi
+  if [[ $DOWNLOAD_SHA256 != $SHA256 ]]; then
+    echo "       **ERROR** SHA256 mismatch: got $DOWNLOAD_SHA256 expected $SHA256"
+    exit 1
+  fi
 
-  # tar xfz /tmp/module.tar.gz -C $DepDir
-  # mv "$DepDir/node_exporter-${VERSION}.linux-amd64" $InstallDir
-  # rm /tmp/module.tar.gz
+  tar xfz /tmp/module.tar.gz -C $DepDir
+  mv "$DepDir/telegraf-${VERSION}_linux_amd64" $InstallDir
+  rm /tmp/module.tar.gz
 
-  mv "$BUILDPACK_DIR/src/telegraf" $InstallDir
+  # mv "$BUILDPACK_DIR/src/telegraf" $InstallDir
   
   mkdir -p "$DepDir/bin"
   cd $DepDir/bin;
